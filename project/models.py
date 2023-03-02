550d4ba7c5d2fa4fa2fe58a860fff30140cd0c52
akula raghuram <akularaghuram55@protonmail.com>
2023-03-03 00:31:31 +0530

    Django Project Added

---
+++ project/models.py
@@ -0,0 +1,13 @@
+from django.db import connections
+from django.db import models
+
+# Create your models here.
+
+class OrderHistory(models.Model):
+    name = models.CharField(max_length=64)
+    orders = models.JSONField()
+    email = models.CharField(max_length=64)
+    payment = models.IntegerField()
+    payment_method = models.CharField(max_length=64)
+    payment_id = models.CharField(max_length=64)
+    date_of_order = models.DateTimeField()
